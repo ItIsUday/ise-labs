590cf32be821f7ab368f0b46f65a8feba0f8c7d2
Uday <iamuday1618@gmail.com>
2021-02-16 20:54:29 +0530

    Improvements

--- OS/cpu_scheduling_multilevel.c
+++ OS/cpu_scheduling_multilevel.c
@@ -1,69 +1,69 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-#define sf scanf
-#define pf printf
-
 int p;
-void fcfs(int q[], int initialwaittime, int np) {
-    int turn[np], wait[np], turnsum = initialwaittime + q[0], waitsum = initialwaittime, i;
-    turn[0] = q[0] + initialwaittime;
-    wait[0] = initialwaittime;
 
-    for (i = 1; i < np; i++) {
-        wait[i] = wait[i - 1] + q[i - 1];
-        turn[i] = wait[i] + q[i];
-        turnsum += turn[i];
-        waitsum += wait[i];
+void fcfs(int *burst_time, int initial_waiting_time, int process_count) {
+    int total_turnaround_time = 0, total_waiting_time = 0;
+
+    printf("\tProcess\tBurst time\tWaiting time\tTurnaround time\n");
+    for (int waiting_time, turnaround_time = initial_waiting_time, i = 0; i < process_count; i++) {
+        turnaround_time += burst_time[i];
+        waiting_time = turnaround_time - burst_time[i];
+        total_waiting_time += waiting_time;
+        total_turnaround_time += turnaround_time;
+        printf("\tP%d\t%d\t\t%d\t\t%d\n", i + 1, burst_time[i], waiting_time, turnaround_time);
     }
 
-    printf("\nProcess\t\tBurst time\t\tWaiting time\t\tTurnaround time\n");
-    for (int i = 0; i < np; i++)
-        printf("P%d\t\t%d\t\t\t%d\t\t\t%d\n", p++, q[i], wait[i], turn[i]);
-    printf("\nAverage Waiting time: %.2f\nAverage Turn-around time: %.2f\n\n\n", (float)waitsum / np, (float)turnsum / np);
+    printf("\n\tThe average waiting time is %0.2f\n",
+           (float) total_waiting_time / (float) process_count);
+    printf("\tThe average turnaround time is %0.2f\n",
+           (float) total_turnaround_time / (float) process_count);
 }
 
 int main() {
     int p1, p2;
-    pf("Enter priority for Queue 1: ");
-    sf("%d", &p1);
-    pf("Enter priority for Queue 2: ");
-    sf("%d", &p2);
+    printf("Enter priority for Queue 1: ");
+    scanf("%d", &p1);
+    printf("Enter priority for Queue 2: ");
+    scanf("%d", &p2);
     int np1, np2, i, w1 = 0, w2 = 0;
     if (p1 == p2) {
-        pf("Priority cannot be the same\n");
-        exit(0);
+        printf("Priority cannot be the same\n");
+        exit(1);
     }
 
-    pf("Enter the number of process of Queue 1: ");
-    sf("%d", &np1);
+    printf("Queue 1\n");
+    printf("\tEnter the number of process: ");
+    scanf("%d", &np1);
     int q1[np1];
     for (i = 0; i < np1; i++) {
-        pf("Enter the burst time for Process %d: ", i + 1);
-        sf("%d", &q1[i]);
+        printf("\tEnter the burst time for Process %d: ", i + 1);
+        scanf("%d", &q1[i]);
         w1 += q1[i];
     }
 
-    pf("Enter the number of process of Queue 2: ");
-    sf("%d", &np2);
+    printf("Queue 2\n");
+    printf("\tEnter the number of process: ");
+    scanf("%d", &np2);
     int q2[np2];
     for (i = 0; i < np2; i++) {
-        pf("Enter the burst time for Process %d: ", i + np1 + 1);
-        sf("%d", &q2[i]);
+        printf("\tEnter the burst time for Process %d: ", i + np1 + 1);
+        scanf("%d", &q2[i]);
         w2 += q2[i];
     }
-    
+
     if (p1 < p2) {
-        pf("\nQUEUE 1:\n");
+        printf("\nQueue 1:\n");
         p = 1;
         fcfs(q1, 0, np1);
-        pf("\nQUEUE 2:\n");
+        printf("\nQueue 2:\n");
         fcfs(q2, w1, np2);
     } else {
-        pf("\nQUEUE 2:\n");
+        printf("\nQueue 2:\n");
         p = np1 + 1;
         fcfs(q2, 0, np2);
-        pf("\nQUEUE 1:\n");
+        printf("\nQueue 1:\n");
         p = 1;
         fcfs(q1, w2, np1);
     }
