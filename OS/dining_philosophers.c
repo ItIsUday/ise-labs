0b07c14e2ebadd4a2c112622ea4e9051d6c88af8
Uday <iamuday1618@gmail.com>
2021-02-18 16:54:40 +0530

    Small improvements
    - Better readability
    - Give philosophers time to think

--- OS/dining_philosophers.c
+++ OS/dining_philosophers.c
@@ -6,28 +6,29 @@
 sem_t seat;
 sem_t chopstick[5];
 
-void *philosopher(void *num) {
-    int phil = *(int *) num;
+void *live_life(void *person) {
+    int philosopher = *(int *) person;
 
     sem_wait(&seat);
-    printf("Philosopher %d has taken seat\n", phil);
-    sem_wait(&chopstick[phil]);
-    sem_wait(&chopstick[(phil + 1) % 5]);
+    printf("Philosopher %d has taken seat\n", philosopher);
+    sem_wait(&chopstick[philosopher]);
+    sem_wait(&chopstick[(philosopher + 1) % 5]);
 
-    printf("Philosopher %d is eating\n", phil);
+    printf("Philosopher %d is eating\n", philosopher);
     sleep(1);
-    printf("Philosopher %d has finished eating\n", phil);
+    printf("Philosopher %d finished eating and is now thinking\n", philosopher);
+    sleep(1);
 
-    sem_post(&chopstick[(phil + 1) % 5]);
-    sem_post(&chopstick[phil]);
+    sem_post(&chopstick[(philosopher + 1) % 5]);
+    sem_post(&chopstick[philosopher]);
     sem_post(&seat);
 
     return NULL;
 }
 
 int main() {
-    int i, name[5];
-    pthread_t tid[5];
+    int i, philosophers[5];
+    pthread_t threads[5];
     printf("\t\tDining Philosophers problem using semaphores\n");
 
     sem_init(&seat, 0, 4);
@@ -35,11 +36,11 @@ int main() {
         sem_init(&chopstick[i], 0, 1);
 
     for (i = 0; i < 5; i++) {
-        name[i] = i;
-        pthread_create(&tid[i], NULL, philosopher, (void *) &name[i]);
+        philosophers[i] = i;
+        pthread_create(&threads[i], NULL, live_life, (void *) &philosophers[i]);
     }
     for (i = 0; i < 5; i++)
-        pthread_join(tid[i], NULL);
+        pthread_join(threads[i], NULL);
 
     sem_destroy(&seat);
     for (i = 0; i < 5; i++)
