9570b01c6825f67e9ba542282b93c946b18839cb
Uday <iamuday1618@gmail.com>
2021-07-05 18:11:45 +0530

    Daemon program now logs at regular intervals

--- USP/daemon.c
+++ USP/daemon.c
@@ -3,6 +3,32 @@
 #include <unistd.h>
 #include <sys/stat.h>
 
+void daemonize();
+void login(FILE *fp);
+
+int main() {
+    daemonize();
+
+    FILE *fp = fopen("/tmp/log.txt", "w+");
+    int count = 0;
+
+    while (count < 10) {
+        sleep(2);
+        fprintf(fp, "Logging info...\n");
+        count++;
+        fflush(fp);
+        if (count == 5)
+            login(fp);
+    }
+
+    fclose(fp);
+}
+
+void login(FILE *fp) {
+    fprintf(fp, "Login called...\n");
+    fflush(fp);
+}
+
 void daemonize() {
     pid_t pid;
 
@@ -28,9 +54,4 @@ void daemonize() {
     freopen("/dev/null", "r", stdin);
     freopen("/dev/null", "w", stdout);
     freopen("/dev/null", "w", stderr);
-}
-
-int main() {
-    daemonize();
-    sleep(20);
 }
